90a63edfc569c0084f86a7824d1a0c2849ac769a
Manuel <manuelrmauro@gmail.com>
2021-12-22 20:57:22 -0300

    redux

---
+++ src/redux/actions.js
@@ -0,0 +1,10 @@
+export const CHANGE_MODE = 'CHANGE_MODE'
+export const CHANGE_LANG = 'CHANGE_LANG'
+
+export function changeMode() {
+	return {type: CHANGE_MODE}
+}
+
+export function changeLang() {
+	return {type: CHANGE_LANG}
+}
---
+++ src/redux/reducer.js
@@ -0,0 +1,27 @@
+import { CHANGE_MODE, CHANGE_LANG } from './actions';
+
+const initialState = {
+	mode: 'lightMode',
+	language: 'EN',
+};
+
+function rootReducer(state = initialState, { type, payload }) {
+	switch (type) {
+		case CHANGE_MODE:
+			if (state.mode === 'lightMode') {
+				return { ...state, mode: 'darkMode' };
+			} else {
+				return { ...state, mode: 'lightMode' };
+			}
+		case CHANGE_LANG:
+			if (state.language === 'EN') {
+				return { ...state, mode: 'ES' };
+			} else {
+				return { ...state, mode: 'EN' };
+			}
+		default:
+			return state;
+	}
+}
+
+export default rootReducer;
