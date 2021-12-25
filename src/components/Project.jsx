95e9f60467583954c27b5440ab6b375dbc0ff758
Manuel <manuelrmauro@gmail.com>
2021-12-25 15:20:01 -0300

    data actualize

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -2,31 +2,38 @@ import React from 'react';
 import { connect } from 'react-redux';
 import ReactPlayer from 'react-player';
 import './Project.css';
-import {BsGithub} from 'react-icons/bs'
+import { BsGithub } from 'react-icons/bs';
+import parse from 'html-react-parser';
 
-
-function Project({ name, summary, video, screenshots, github , link}) {
+function Project({ name, summary, video, screenshots, github, link }) {
 	return (
 		<div className="project">
 			<h3>{name}</h3>
-			<p>{summary}</p>
-			{link.length ? <a href={link} target="blank"><p>Link to App</p></a>: null}
-			<a href={github} target="blank"><p><BsGithub/>{github.replace('https://github.com/manuelrmauro','')}</p></a>
-			<div className="videoWraper">
-				<ReactPlayer
-					width='100%'
-					height='100%'
-					className="projectVideo"
-					url={video}
-				/>
-			</div>
-				
+			<p>{parse(summary)}</p>
+			{link.length ? (
+				<a href={link} target="blank">
+					<p>App Online</p>
+				</a>
+			) : null}
+			<a href={github} target="blank">
+				<p>
+					<BsGithub />
+					{github.replace('https://github.com/manuelrmauro', '')}
+				</p>
+			</a>
+			{video.length ? (
+				<div className="videoWraper">
+					<ReactPlayer
+						width="100%"
+						height="100%"
+						className="projectVideo"
+						url={video}
+						playsinline={true}
+					/>
+				</div>
+			) : null}
 		</div>
 	);
 }
 
-const mapStateToProps = (state) => ({});
-
-const mapDispatchToProps = {};
-
-export default connect(mapStateToProps, null)(Project);
+export default connect(null, null)(Project);
